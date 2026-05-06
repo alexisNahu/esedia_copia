a4f9588042444221a996e83b0510c18294966427
anig <alexisnahuelidoyaga@gmail.com>
2026-05-06 13:58:52 -0300

    adaptando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // <--- Asegúrate de agregar esta línea
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
+
+  adapter: vercel(),
 });
--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -1,14 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    // Mantienes tu configuración de alias para Vite
     vite: {
-      resolve: {
-          alias: {
-              '@':'/src'
-          }
-      }
+        resolve: {
+            alias: {
+                '@':'/src'
+            }
+        }
     },
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     ],
+    theme: {
+        extend: {
+            fontFamily: {
+                // 'jakarta' se usará como la clase font-jakarta
+                'jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+                // 'opensans' se usará como la clase font-opensans
+                'opensans': ['"Open Sans"', 'sans-serif'],
+
+                'newsreader': ['"Newsreader"', 'sans-serif']
+            },
+            colors: {
+                grisClaroFondo: '#A6A6A6',
+                grisClaroTextoB: "#5B6063",
+                grisClaroTextoA: '#64748B',
+                azulOscuro: '#2F638F',
+                celesteClaro: "#9BCBFD",
+            },
+        },
+    },
     plugins: [],
 }
